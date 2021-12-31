71584d6748bfdb162cfd2c5421c488beab87b0f9
SzuYing Lin <96858718+HermyLin@users.noreply.github.com>
2022-01-01 01:14:45 +0800

    Update Flex_template.py

--- msg_template/Flex_template.py
+++ msg_template/Flex_template.py
@@ -1,153 +1,113 @@
 from linebot.models import FlexSendMessage
 def dayornight():
     flex_message = FlexSendMessage(
-                   alt_text='選擇時段',
-                   contents= {
-                                "type": "bubble",
-                                "hero": {
-                                "type": "image",
-                                "url": "https://github.com/HermyLin/stealer/blob/main/bot_image/images.png",
-                                "size": "full",
-                                "aspectRatio": "20:13",
-                                "aspectMode": "cover",
-                                "position": "relative"
-                                        },
-                                "body": {
-                                "type": "box",
-                                "layout": "vertical",
-                                "contents": [
-                                            {
-                                            "type": "text",
-                                            "text": "請選擇時辰",
-                                            "weight": "bold",
-                                            "size": "xl",
-                                            "contents": [],
-                                            "color": "#876C5A"
-                                            },    
-                                            {
-                                            "type": "text",
-                                            "text": "貼心提醒：\n上午>00:00-11:59\n下午>12:00/23:59",
-                                            "size": "xs",
-                                            "margin": "sm"
-                                            },
-                                            {
-                                            "type": "separator",
-                                            "margin": "lg"
-                                            },
-                                            {
-                                            "type": "box",
-                                            "layout": "verical",
-                                            "contents": [
-                                                        {
-                                                        "type": "button",
-                                                        "action": {
-                                                        "type": "message",
-                                                        "label": "上午",
-                                                        "text": "上午"
-                                                        },
-                                                        "style": "primary",
-                                                        "height": "sm",
-                                                        "color": "#797D62"
-                                                        },
-                                                        {
-                                                        "type": "button",
-                                                        "action": {
-                                                        "type": "message",
-                                                        "label": "下午",
-                                                        "text": "下午"
-                                                        },
-                                                        "style": "primary",
-                                                        "margin": "xxl",
-                                                        "height": "sm",
-                                                        "color": "#797D62"
-                                                        }
-                                                        ],
-                                            "margin": "md"
-                                            }
-                                            ]
-                                        }  
-                            }                                        
-                                )                                
-    return flex_message
-
-def cartype_choose():
-    flex_message = FlexSendMessage(
-                   alt_text='選擇車型',
-                   contents= {
-                                "type": "bubble",
-                                "hero": {
-                                "type": "image",
-                                "url": "https://images.twgreatdaily.com/images/elastic/iuP/iuPP1mwBJleJMoPMp5x0.jpg",
-                                "size": "full",
-                                "aspectRatio": "20:13",
-                                "aspectMode": "cover",
-                                "position": "relative"
-                                        },
-                                "body": {
-                                "type": "box",
-                                "layout": "vertical",
-                                "contents": [
-                                            {
-                                            "type": "text",
-                                            "text": "請選擇車型",
-                                            "weight": "bold",
-                                            "size": "xl",
-                                            "contents": [],
-                                            "color": "#876C5A"
-                                            },    
-                                            #{
-                                            #"type": "text",
-                                            #"text": "",
-                                            #"size": "xs",
-                                            #"margin": "sm"
-                                            #},
-                                            {
-                                            "type": "separator",
-                                            "margin": "lg"
-                                            },
-                                            {
-                                            "type": "box",
-                                            "layout": "verical",
-                                            "contents": [
-                                                        {
-                                                        "type": "button",
-                                                        "action": {
-                                                        "type": "message",
-                                                        "label": "汽車\udbc0\udc49",
-                                                        "text": "汽車"
-                                                        },
-                                                        "style": "primary",
-                                                        "height": "sm",
-                                                        "color": "#797D62"
-                                                        },
-                                                        {
-                                                        "type": "button",
-                                                        "action": {
-                                                        "type": "message",
-                                                        "label": "機車\udbc0\udc8c",
-                                                        "text": "機車"
-                                                        },
-                                                        "style": "primary",
-                                                        "height": "sm",
-                                                        "color": "#797D62"
-                                                        },
-                                                        {
-                                                        "type": "button",
-                                                        "action": {
-                                                        "type": "message",
-                                                        "label": "腳踏車\udbc0\udc4b",
-                                                        "text": "腳踏車"
-                                                        },
-                                                        "style": "primary",
-                                                        "margin": "xxl",
-                                                        "height": "sm",
-                                                        "color": "#797D62"
-                                                        }
-                                                        ],
-                                            "margin": "md"
-                                            }
-                                            ]
-                                        }
-                            }                                        
-                                )                       
+        alt_text='選擇時段',
+        contents= {
+  "type": "bubble",
+  "hero": {
+    "type": "image",
+    "url": "https://github.com/HermyLin/stealer/blob/main/bot_image/images.png",
+    "size": "full",
+    "aspectRatio": "20:13",
+    "aspectMode": "cover",
+    "margin": "none",
+    "position": "relative",
+    "gravity": "top"
+  },
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "請選擇時辰",
+        "weight": "bold",
+        "size": "xl",
+        "color": "#00bfff",
+        "margin": "none"
+      },
+      {
+        "type": "box",
+        "layout": "baseline",
+        "margin": "xs",
+        "contents": [
+          {
+            "type": "icon",
+            "size": "sm",
+            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
+            "margin": "none"
+          },
+          {
+            "type": "text",
+            "text": "貼心提醒：",
+            "size": "sm",
+            "color": "#999999",
+            "margin": "md",
+          }
+        ]
+      },
+      {
+        "type": "text",
+        "text": "上午 → 00:00-11:59",
+        "size": "sm",
+        "color": "#999999",
+        "margin": "none"
+      },
+      {
+        "type": "text",
+        "text": "下午 → 12:00-23:59",
+        "size": "sm",
+        "color": "#999999",
+        "margin": "none"
+      }
+    ],
+    "position": "relative",
+    "margin": "none"
+  },
+  "footer": {
+    "type": "box",
+    "layout": "horizontal",
+    "spacing": "md",
+    "contents": [
+      {
+        "type": "button",
+        "style": "primary",
+        "height": "sm",
+        "action": {
+          "type": "message",
+          "label": "上午",
+          "text": "上午"
+        },
+        "margin": "none",
+        "color": "#deb887"
+      },
+      {
+        "type": "button",
+        "style": "primary",
+        "height": "sm",
+        "action": {
+          "type": "message",
+          "label": "下午",
+          "text": "下午"
+        },
+        "color": "#deb887"
+      },
+      {
+        "type": "spacer",
+        "size": "xs"
+      }
+    ],
+    "flex": 0,
+    "borderWidth": "none",
+    "cornerRadius": "none",
+    "margin": "xs",
+    "paddingAll": "lg"
+  },
+  "styles": {
+    "footer": {
+      "separator": true
+    }
+  }
+}                                       
+        )                       
     return flex_message
