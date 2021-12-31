a4529356922b67ba348d3291bcd74073149e4b5f
SzuYing Lin <96858718+HermyLin@users.noreply.github.com>
2021-12-31 16:08:31 +0800

    Update test.py

--- test.py
+++ test.py
@@ -51,7 +51,24 @@ def callback():
 
 #訊息傳遞區塊
 ##### 基本上程式編輯都在這個function #####
-@handler.add(MessageEvent, message=TextMessage) 
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    if event.message.text in == "機車":
+        car_type = event.message.text    
+        text_message_location = TextSendMessage(text='請分享位置給我，讓我守護您愛車的安全\udbc0\udc2e', 
+                                quick_reply=QuickReply(items=[
+                                QuickReplyButton(action=LocationAction(label="點點我分享"))]))    
+        line_bot_api.reply_message(event.reply_token,text_message_location)
+    elif event.message.text == "問題回報":
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text = "先這樣"))
+        #line_bot_api.reply_message(event.reply_token,TextSendMessage(text = "先這樣"))
+        #line_bot_api.reply_message(event.reply_token,TextSendMessage(text= Text_template.user_report()))
+        #製作問題回報表單(google)
+    else:
+        line_bot_api.reply_message(event.reply_token,"Oops!小守找不到您的資訊呢～")
+        #line_bot_api.reply_message(event.reply_token,TextSendMessage(text="Oops!小守找不到您的資訊呢～"))
+        #line_bot_api.reply_message(event.reply_token,TextSendMessage(text = Text_template.keyword_warning_text()))
+'''
 def handle_message(event):
     if event.message.text == "定位":
         text_message_location = TextSendMessage(text='偷偷分享位置給我，我才能守護你的安全喔！\udbc0\udc2e',
@@ -62,7 +79,7 @@ def handle_message(event):
     else:
         message = TextSendMessage(text=event.message.text)
         line_bot_api.reply_message(event.reply_token,message)
-
+'''
 @handler.add(MessageEvent, message=LocationMessage)
 def handle_location_message(event):
     u_lat = event.message.latitude  #緯度
