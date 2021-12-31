b805f85694e46046b6460cb6891e6e078508a145
SzuYing Lin <96858718+HermyLin@users.noreply.github.com>
2022-01-01 02:20:08 +0800

    Add files via upload

--- msg_template/Flex_template.py
+++ msg_template/Flex_template.py
@@ -6,7 +6,7 @@ def dayornight():
   "type": "bubble",
   "hero": {
     "type": "image",
-    "url": "https://transcode-v2.app.engoo.com/image/fetch/f_auto,c_limit,w_1280,h_800,dpr_2/https://assets.app.engoo.com/images/CGPkj72Wn3gPmu9ebqmpS1nGQNOZQlR70NilqMAWUBm.png",
+    "url": "https://github.com/HermyLin/stealer/blob/main/bot_image/images.png",
     "size": "full",
     "aspectRatio": "20:13",
     "aspectMode": "cover",
@@ -58,7 +58,6 @@ def dayornight():
             "size": "xs",
             "color": "#999999",
             "margin": "none",
-            "flex": 0
           }
         ],
         "spacing": "none"
@@ -115,15 +114,117 @@ def dayornight():
         "size": "xs"
       }
     ],
-    "flex": 0,
     "borderWidth": "none",
-    "cornerRadius": "none",
     "margin": "xs",
     "paddingAll": "lg"
   },
   "styles": {
     "footer": {
       "separator": True
+    }
+  }
+}
+    )                       
+    return flex_message
+
+def cartype_choose(reply_text):
+    flex_message = FlexSendMessage(
+                    alt_text=reply_text,
+                    contents={
+  "type": "bubble",
+  "hero": {
+    "type": "image",
+    "url": "https://images.twgreatdaily.com/images/elastic/iuP/iuPP1mwBJleJMoPMp5x0.jpg",
+    "size": "full",
+    "aspectRatio": "20:13",
+    "aspectMode": "cover",
+    "margin": "none",
+    "position": "relative",
+    "gravity": "top"
+  },
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "請選擇車種",
+        "weight": "bold",
+        "size": "xl",
+        "color": "#00bfff",
+        "margin": "none"
+      },
+      {
+        "type": "box",
+        "layout": "baseline",
+        "margin": "xs",
+        "contents": [
+          {
+            "type": "text",
+            "size": "sm",
+            "color": "#999999",
+            "margin": "none",
+            "text": "要停放什麼車呢？"
+          }
+        ]
+      }
+    ],
+    "position": "relative",
+    "margin": "none"
+  },
+  "footer": {
+    "type": "box",
+    "layout": "vertical",
+    "spacing": "md",
+    "contents": [
+      {
+        "type": "button",
+        "style": "primary",
+        "action": {
+          "type": "message",
+          "label": "汽車",
+          "text": "汽車"
+        },
+        "margin": "none",
+        "color": "#deb887",
+        "height": "sm",
+        "position": "relative"
+      },
+      {
+        "type": "button",
+        "style": "primary",
+        "height": "sm",
+        "action": {
+          "type": "message",
+          "label": "機車",
+          "text": "機車"
+        },
+        "color": "#deb887"
+      },
+      {
+        "type": "button",
+        "style": "primary",
+        "height": "sm",
+        "action": {
+          "type": "message",
+          "label": "腳踏車",
+          "text": "腳踏車"
+        },
+        "color": "#deb887"
+      }
+    ],
+    "borderWidth": "none",
+    "cornerRadius": "none",
+    "margin": "xs",
+    "paddingAll": "lg",
+    "paddingTop": "md",
+    "paddingBottom": "lg",
+    "paddingStart": "xxl",
+    "paddingEnd": "xxl"
+  },
+  "styles": {
+    "footer": {
+      "separator": true
     }
   }
 }
