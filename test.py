9c77fe81ce0ec52c31fcd4fa34eda9e6b640d0c1
SzuYing Lin <96858718+HermyLin@users.noreply.github.com>
2022-01-01 00:04:41 +0800

    Update test.py

--- test.py
+++ test.py
@@ -18,13 +18,35 @@ from linebot.exceptions import (
 )
 from linebot.models import *
 
+import os
+import time
+import json
+import csv
+import re
+import requests
+import pandas as pd
+
+#載入我們的py檔
+from msg_template import Flex_template, Result_flex
+
+
+#------------------------------------------
+# 定義function與基礎設置
+#---------------car type-------------------
+car_type_list = ["汽車","機車","腳踏車"]
+#----------------時間設定-------------------
+time_label = ['上午','下午']
+times = ['00:00-01:59','02:00-03:59','04:00-05:59','06:00-07:59','08:00-09:59','10:00-11:59','12:00-13:59','14:00-15:59','16:00-17:59','18:00-19:59','20:00-21:59','22:00-23:59']
+#------------------------------------------
+
+
+#----------------------------------------------
+#LINE聊天機器人的基本資料
 app = Flask(__name__)
 
-# 必須放上自己的Channel Access Token
 #line_bot_api = LineBotApi('你自己的token')
 line_bot_api = LineBotApi('g5sIQeW/GQj6y3+giEq5X85VaQzPodMv6D0o4PWKJQxezU+3IX++uqqMLOsgWDJ7EIdTeCInkRBAgMQXAfqPhWLvo+/k2yqKewC63eZORQxJsgiUt9FmIQmwDmGmgnBGaS/usc7T6EV4/uFv6E1eDQdB04t89/1O/w1cDnyilFU=')
 
-# 必須放上自己的Channel Secret
 #handler = WebhookHandler('你自己的secret')
 handler = WebhookHandler('ae8a10ffbeda85e0e00dcbb41f4f9f47')
 
@@ -54,12 +76,16 @@ def callback():
 ##### 基本上程式編輯都在這個function #####
 @handler.add(MessageEvent, message=TextMessage) 
 def handle_message(event):
-    if event.message.text == "定位":
+    if event.message.text in car_type_list:
         text_message_location = TextSendMessage(text='偷偷分享位置給我，我才能守護你的安全喔！\udbc0\udc2e',
                                                 quick_reply=QuickReply(items=[
                                                 QuickReplyButton(action=LocationAction(label="點點我分享"))]))
         line_bot_api.reply_message(event.reply_token,text_message_location)
 
+    elif event.message.text == "問題回報":
+        message = TextSendMessage(text="no problem!")
+        line_bot_api.reply_message(event.reply_token,message)
+        
     else:
         message = TextSendMessage(text=event.message.text)
         line_bot_api.reply_message(event.reply_token,message)
