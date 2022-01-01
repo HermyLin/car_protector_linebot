f7b9e1717762049316b24b4fa61cac4999331064
SzuYing Lin <96858718+HermyLin@users.noreply.github.com>
2022-01-01 22:33:46 +0800

    Update Text_template.py

--- msg_template/Text_template.py
+++ msg_template/Text_template.py
@@ -1,10 +1,10 @@
 def problems_report():
-    report_text =f"\udbc0\udc31常見問題\n\n• 按鈕沒有回應：\n可能是小守睡著了，請多點幾次呦！\udbc0\udc8e\udbc0\udc8e\
+    report_text =f"\udbc0\udc31常見問題\n\n• 按鈕沒有回應：\n可能是小守睡著了，請多點幾次呦！\
                     \n\
-                    \n• 目前還未開放打字功能喔～\n小守團隊未來會繼續努力的！\
+                    \n• 目前還未開放打字功能喔！\n  小守團隊正在努力開發中~\
                     \n\
-                    \n• 未來有機會會開放地區搜尋的功能～\
+                    \n• 未來有機會會開放地區搜尋的功能~\
                     \n\
-                    \n歡迎填寫使用者回饋表單，幫助小守團隊改進！\n非常感謝您\udbc0\udc7a\\udbc0\udc7a\\udbc0\udc7a\
+                    \n歡迎填寫回饋表單，幫助小守團隊改進！\n非常感謝您！\udbc0\udc7a\udbc0\udc7a\udbc0\udc7a\
                     \nhttps://reurl.cc/Npa596"
     return report_text 
