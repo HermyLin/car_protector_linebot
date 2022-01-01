bcd0228648d4ed5af7f919469ce7b0c33d50b74f
SzuYing Lin <96858718+HermyLin@users.noreply.github.com>
2022-01-01 17:23:34 +0800

    Update test.py

--- test.py
+++ test.py
@@ -79,126 +79,122 @@ def handle_message(event):
         flex_message_time = FlexSendMessage(
             alt_text='pick time',
             contents={
-  "type": "bubble",
-  "hero": {
-    "type": "image",
-    "url": "https://github.com/HermyLin/stealer/blob/main/bot_image/images.png",
-    "size": "full",
-    "aspectRatio": "20:13",
-    "aspectMode": "cover",
-    "margin": "none",
-    "position": "relative",
-  },
-  "body": {
-    "type": "box",
-    "layout": "vertical",
-    "contents": [
-      {
-        "type": "text",
-        "text": "請選擇時段",
-        "weight": "bold",
-        "size": "xl",
-        "color": "#00bfff",
-        "margin": "none"
-      },
-      {
-        "type": "box",
-        "layout": "baseline",
-        "margin": "xs",
-        "contents": [
-          {
-            "type": "icon",
-            "size": "xs",
-            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
-            "margin": "none",
-            "position": "relative"
-          },
-          {
-            "type": "text",
-            "size": "sm",
-            "color": "#999999",
-            "margin": "xs",
-            "text": "貼心提醒："
-          }
-        ]
-      },
-      {
-        "type": "box",
-        "layout": "baseline",
-        "margin": "xs",
-        "contents": [
-          {
-            "type": "text",
-            "text": "上午為 00:00－11:59",
-            "size": "xs",
-            "color": "#999999",
-            "margin": "none",
-          }
-        ],
-        "spacing": "none"
-      },
-      {
-        "type": "box",
-        "layout": "baseline",
-        "margin": "xs",
-        "contents": [
-          {
-            "type": "text",
-            "text": "下午為 12:00－23:59",
-            "size": "xs",
-            "color": "#999999",
-            "margin": "none"
-          }
-        ],
-        "spacing": "none"
-      }
-    ],
-    "position": "relative",
-    "margin": "none"
-  },
-  "footer": {
-    "type": "box",
-    "layout": "horizontal",
-    "spacing": "md",
-    "contents": [
-      {
-        "type": "button",
-        "style": "primary",
-        "height": "sm",
-        "action": {
-          "type": "message",
-          "label": "上午",
-          "text": "上午"
-        },
-        "margin": "none",
-        "color": "#deb887"
-      },
-      {
-        "type": "button",
-        "style": "primary",
-        "height": "sm",
-        "action": {
-          "type": "message",
-          "label": "下午",
-          "text": "下午"
-        },
-        "color": "#deb887"
-      },
-      {
-        "type": "spacer",
-        "size": "xs"
-      }
-    ],
-    "borderWidth": "none",
-    "margin": "xs",
-    "paddingAll": "lg"
-  },
-  "styles": {
-    "footer": {
-      "separator": True
-    }
-  }
-}
+                                "type": "bubble",
+                                "hero": {
+                                  "type": "image",
+                                  "url": "https://github.com/yumei86/iRamen_linebot/blob/master/image/TWmap.png?raw=true",
+                                  "size": "full",
+                                  "aspectRatio": "20:13",
+                                  "aspectMode": "cover",
+                                  "position": "relative"
+                                },
+                                "body": {
+                                  "type": "box",
+                                  "layout": "vertical",
+                                  "contents": [
+                                    {
+                                      "type": "text",
+                                      "text": "你在哪裡？",
+                                      "weight": "bold",
+                                      "size": "xl",
+                                      "contents": [],
+                                      "color": "#876C5A"
+                                    },
+                                    {
+                                      "type": "text",
+                                      "text": "請幫助我選擇你在台灣的哪個位置：）",
+                                      "size": "xs",
+                                      "margin": "sm"
+                                    },
+                                    {
+                                      "type": "separator",
+                                      "margin": "lg"
+                                    },
+                                    {
+                                      "type": "box",
+                                      "layout": "horizontal",
+                                      "contents": [
+                                        {
+                                          "type": "button",
+                                          "action": {
+                                            "type": "message",
+                                            "label": "北部",
+                                            "text": "北部"
+                                          },
+                                          "style": "primary",
+                                          "height": "sm",
+                                          "color": "#797D62"
+                                        },
+                                        {
+                                          "type": "button",
+                                          "action": {
+                                            "type": "message",
+                                            "label": "中部",
+                                            "text": "中部"
+                                          },
+                                          "style": "primary",
+                                          "margin": "xxl",
+                                          "height": "sm",
+                                          "color": "#797D62"
+                                        }
+                                      ],
+                                      "margin": "lg"
+                                    },
+                                    {
+                                      "type": "box",
+                                      "layout": "horizontal",
+                                      "contents": [
+                                        {
+                                          "type": "button",
+                                          "action": {
+                                            "type": "message",
+                                            "label": "南部",
+                                            "text": "南部"
+                                          },
+                                          "style": "primary",
+                                          "height": "sm",
+                                          "color": "#797D62"
+                                        },
+                                        {
+                                          "type": "button",
+                                          "action": {
+                                            "type": "message",
+                                            "label": "東部",
+                                            "text": "東部"
+                                          },
+                                          "style": "primary",
+                                          "margin": "xxl",
+                                          "height": "sm",
+                                          "color": "#797D62"
+                                        }
+                                      ],
+                                      "margin": "md"
+                                    },
+                                    {
+                                      "type": "separator",
+                                      "margin": "lg"
+                                    },
+                                    {
+                                      "type": "box",
+                                      "layout": "vertical",
+                                      "contents": [
+                                        {
+                                          "type": "button",
+                                          "action": {
+                                            "type": "message",
+                                            "label": "開定位探店GPS",
+                                            "text": "定位"
+                                          },
+                                          "style": "primary",
+                                          "margin": "xxl",
+                                          "height": "sm",
+                                          "color": "#797D62"
+                                        }
+                                      ]
+                                    }
+                                  ]
+                                }
+                    }
     )                       
         line_bot_api.reply_message(event.reply_token,flex_message_time)
     
